ee3e1601e7fd503c1431289f1bf480a7ec8b3fb5
Li Jiayang <142671113+endercat-alu@users.noreply.github.com>
2024-08-16 13:20:52 +0800

    Update discourse-location.user.js

--- discourse-location.user.js
+++ discourse-location.user.js
@@ -5,6 +5,8 @@
 // @description  自动获取用户当前位置并更新到 Discourse 个人资料中。
 // @author       鹿目 まどか Advanced
 // @match        https://linux.do/*
+// @icon         https://haojiezhe12345.top:82/madohomu/res/favicon-320.png
+// @license      MIT
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
